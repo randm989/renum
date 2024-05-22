bda32da1f82f5cb5b7deb95ba455f76b9c2d5c9f
Josh Rutz <joshrutz989@gmail.com>
2024-05-22 00:29:14 -0400

    last change for the night

--- Code/Headers/SmartEnums.hpp
+++ Code/Headers/SmartEnums.hpp
@@ -3,20 +3,39 @@
 #include <string>
 #include <variant>
 
-namespace EMessages
-{
-	struct SuccessMessage
-	{
+// namespace EMessages
+// {
+// 	struct SuccessMessage
+// 	{
 		
-	};
+// 	};
 
-	struct RerouteMessage
-	{
-		std::string URL;
-	};
+// 	struct RerouteMessage
+// 	{
+// 		std::string URL;
+// 	};
+// }
+
+// Macro to define an empty struct
+#define DEFINE_ENUM_VALUE(Name) struct Name {}
+
+// Macro to define a struct with parameters
+#define DEFINE_ENUM_VALUE_WITH_PARAMS(Name, ...) struct Name { __VA_ARGS__; }
+
+#define DEFINE_SMART_ENUM(NamespaceName, ...) \
+namespace NamespaceName { \
+    __VA_ARGS__ \
 }
 
-using EMessage = std::variant<EMessages::SuccessMessage, EMessages::RerouteMessage>;
+#define DECLARE_SMART_ENUM_INSTANCE_TYPE(Name, ...) \
+using Name = std::variant<__VA_ARGS__>;
+
+DEFINE_SMART_ENUM(EMessages,
+	DEFINE_ENUM_VALUE(SuccessMessage);
+    DEFINE_ENUM_VALUE_WITH_PARAMS(RerouteMessage, std::string URL);
+    DEFINE_ENUM_VALUE_WITH_PARAMS(MultiMessage, std::string URL; int integer);
+)
+DECLARE_SMART_ENUM_INSTANCE_TYPE(EMessage, EMessages::SuccessMessage, EMessages::RerouteMessage, EMessages::MultiMessage)
 
 template <typename... Ts> struct Overloaded : Ts... { using Ts::operator()...; };
 template <typename... Ts> Overloaded(Ts...) -> Overloaded<Ts...>;
--- main.cpp
+++ main.cpp
@@ -11,13 +11,15 @@ int main()
 		{
 			EMessages::RerouteMessage{"ReroutedURL"},
 			EMessages::RerouteMessage{"Rerouted2"},
-			EMessages::SuccessMessage{}
+			EMessages::SuccessMessage{},
+			EMessages::MultiMessage{"google?", 42560}
 		};
 
 	for (const auto& Msg : Msgs)
 		Match(Msg,
 		[](EMessages::SuccessMessage) { cout << "Success!" << endl; },
-		[](EMessages::RerouteMessage msg) { cout << "Reroute: " << msg.URL << endl; });
+		[](EMessages::RerouteMessage msg) { cout << "Reroute: " << msg.URL << endl; },
+		[](EMessages::MultiMessage msg) { cout << "MultiMessage: " << msg.integer << " " << msg.URL << endl; });
 
 	std::cout << "Hello world" << std::endl;
 }
